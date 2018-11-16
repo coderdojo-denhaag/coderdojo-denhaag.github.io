f37e92d68a1db565c60e3728e8d64ff77b329b50
Sander Gerz <sander@devtips.net>
2018-11-16 16:26:31 +0100

    flappybird devicewidth

--- sander/flappybird/main.js
+++ sander/flappybird/main.js
@@ -84,6 +84,7 @@ var mainState = {
 
 // Initialize Phaser, and create a 400px by 490px game
 var game = new Phaser.Game(400, 490);
+game.USE_DEVICE_PIXEL_RATIO = true;
 
 // Add the 'mainState' and call it 'main'
 game.state.add('main', mainState);
